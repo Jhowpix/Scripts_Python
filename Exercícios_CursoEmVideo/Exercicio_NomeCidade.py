00533eddbbd6516bc86c0c384fe030576972989b
Jhonatan de Oliveira Silva <jhonatandevbr@gmail.com>
2026-09-14 16:31:59 -0300

    Exercicios

---
+++ Exercícios_CursoEmVideo/Exercicio_NomeCidade.py
@@ -0,0 +1,4 @@
+# Crie um algoritmo que se o inicio do nome da cidade for Santo retorne True (verdadeiro) se nao False (falso)
+nomeCidade = str(input("Digite um nome de cidade: ")).strip().upper()
+print(nomeCidade == "SANTO")
+
---
+++ Exercícios_CursoEmVideo/Exercicio_VerificarNome.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que confirme a existência do Silva em nome completo
+nomeSilva = str(input("Digite um nome completo: ")).strip().upper()
+nomeSilvaLista = nomeSilva.split()
+print("SILVA" in nomeSilvaLista)
+
+
+
