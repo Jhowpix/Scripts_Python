825b79d3569da0d16a6ea186432a916c606cae6d
Jhonatan de Oliveira Silva <jhonatandevbr@gmail.com>
2026-09-18 00:03:11 -0300

    Exercício

--- Exercícios_CursoEmVideo/Exercicio_Multiplos3.py
+++ Exercícios_CursoEmVideo/Exercicio_Multiplos3.py
@@ -4,6 +4,10 @@ que são múltiplos de três e que se encontram no
 intervalo de 1 até 500.
 '''
 
+conta = 0
+total = 0
 for cont in range(1, 501, 2):
     if cont % 3 == 0:
-        print(cont)
+        conta += cont
+        total += 1
+print('A soma dos números divisíveis por 3 entre 1 e 500 é de {} e a quantidade de valores somados foram de {}'.format(total, conta))
