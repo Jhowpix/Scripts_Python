cb439a46f36e2720ca7752d440a01afa8b2336bb
Jhonatan de Oliveira Silva <jhonatandevbr@gmail.com>
2026-09-17 23:51:19 -0300

    Exercício

---
+++ Exercícios_CursoEmVideo/Exercicio_Multiplos3.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que calcule a soma entre todos os números
+que são múltiplos de três e que se encontram no
+intervalo de 1 até 500.
+'''
+
+for cont in range(1, 501, 2):
+    if cont % 3 == 0:
+        print(cont)
